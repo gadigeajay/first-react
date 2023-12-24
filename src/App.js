ef446bbe1f7aef2dd26201931ff512029c76165e
gadigeajay <gajay42905@gmail.com>
2023-12-24 13:45:18 +0530

    first react app lord shiva

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>yes</h1>
+      <p>hell</p>
+      <img src="https://rukminim1.flixcart.com/image/850/1000/kvtuxa-wall-poster-hd-god-poster-for-room-office-gym-original-imag8nb3kehngdyz.jpeg?q=20" alt="lord-siva"/>
+    </div>
+  );
+}
+
+export default App;
